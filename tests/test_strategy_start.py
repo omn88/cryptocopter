5221a0a32ef24e27231be879fa15aabf92700d27
omn88 <Mikolaj Nizio>
2022-12-10 17:34:32 +0100

    determine start position tested

--- tests/test_strategy_start.py
+++ tests/test_strategy_start.py
@@ -6,11 +6,11 @@ import pandas
 import pytest
 
 from src.backtest.lib import get_futures_historical_data
-from src.producers.producers import determine_start_position
-from src.features import combined_signals_generate, Signals
+from src.producers.producers import determine_start_position, Event
+from src.features import Signals
 from src.workers.workers import print_last_n_rows
 
-# from sample_dataframes import dataframe_short
+from tests.data.sample_dataframes import dataframe_gen
 
 
 @pytest.mark.asyncio
@@ -33,25 +33,26 @@ async def test_get_historical_data(mock_get_historical_klines):
         await client.close_connection()
 
 
-# @pytest.mark.asyncio
-# @patch("src.features.combined_signals_generate")
-# async def test_determine_start_position(mock_df):
-#     client = binance.AsyncClient()
-#     queue = asyncio.Queue()
-#     assert queue.qsize() == 0
-#
-#     try:
-#         mock_df.return_value = dataframe_short()
-#         mock_df = combined_signals_generate(
-#             df=mock_df, condition_lists=[], choice_lists=[]
-#         )
-#
-#         mock_df["position"] = Signals.FLAT
-#
-#         mock_df = await determine_start_position(df=mock_df, queue=queue)
-#
-#         await print_last_n_rows(df=mock_df)
-#
-#         assert queue.qsize() == 1
-#     finally:
-#         await client.close_connection()
+@pytest.mark.parametrize(
+    "signal",
+    [Signals.LONG, Signals.LONG_20, Signals.SHORT, Signals.SHORT_80, Signals.NULL],
+)
+@pytest.mark.asyncio
+async def test_determine_start_position(signal):
+    client = binance.AsyncClient()
+    queue = asyncio.Queue()
+    assert queue.qsize() == 0
+
+    try:
+        df = dataframe_gen(desired_signal=signal)
+        df["position"] = Signals.FLAT
+
+        await determine_start_position(df=df, queue=queue)
+
+        assert queue.qsize() == 1
+        event = await queue.get()
+        assert isinstance(event, Event)
+        assert event.content["last_signal"] == signal
+        assert queue.qsize() == 0
+    finally:
+        await client.close_connection()
