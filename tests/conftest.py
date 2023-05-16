ced0518a36ba2b641516cbb0000e41a9b2a1e81f
omn88 <Mikolaj Nizio>
2023-05-16 13:16:32 +0200

    end this PR with working mypy, tests are for another PR

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,10 @@ import logging
 
 from src.common.common import insert_to_pandas, rsi_indicator_apply
 from src.common.identifiers import Position, Signal
-from src.common.initialize_trading_environment import create_async_queue
+from src.common.initialize_trading_environment import (
+    create_async_queue,
+    create_async_client,
+)
 from src.common.orders import order_quantity_list_prepare
 from src.strategies.rsi_basic import BasicStrategy
 from src.strategies.rsi_extended import ExtendedStrategy
@@ -15,16 +18,16 @@ logger = logging.getLogger("conftest")
 
 
 @pytest.fixture()
-async def basic_rsi(mock_async_client):
+async def basic_rsi():
     raw_data = raw_data_generate(desired_signal=Signal.NULL)
     df = insert_to_pandas(data=raw_data)
     df = rsi_indicator_apply(df=df)
-
+    client = await create_async_client()
     position = Position()
     queue = await create_async_queue()
 
     tsm = BasicStrategy(
-        client=mock_async_client,
+        client=client,
         balance=1000,
         order_quantity_list=order_quantity_list_prepare(),
         df=df,
@@ -40,26 +43,27 @@ async def basic_rsi(mock_async_client):
     await tsm.client.close_connection()
 
 
+# @pytest.fixture
+# def mock_async_client(mocker):
+#     mock_create = mocker.patch("binance.AsyncClient.create", new_callable=AsyncMock)
+#
+#     # Arrange
+#     mock_create.return_value = AsyncMock()
+#
+#     return mock_create
+
+
 @pytest.fixture
-def mock_async_client(mocker):
-    mock_create = mocker.patch("binance.AsyncClient.create", new_callable=AsyncMock)
-
-    # Arrange
-    mock_create.return_value = AsyncMock()
-
-    return mock_create
-
-
-@pytest.fixture
-async def extended_rsi(mock_async_client):
+async def extended_rsi():
     raw_data = raw_data_generate(desired_signal=Signal.NULL)
     df = insert_to_pandas(data=raw_data)
     df = rsi_indicator_apply(df=df)
     position = Position()
+    client = await create_async_client()
     queue = await create_async_queue()
 
     tsm = ExtendedStrategy(
-        client=mock_async_client,
+        client=client,
         balance=1000,
         order_quantity_list=order_quantity_list_prepare(),
         df=df,
