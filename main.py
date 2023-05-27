95363e13f7302c152631c5b35c99dbfff8c6084d
omn88 <Mikolaj Nizio>
2023-05-27 22:40:47 +0200

    introduce main.kv file under src/gui

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.logger import Logger
 from kivy.properties import ObjectProperty, ListProperty, NumericProperty
-from kivy.uix.recycleview import RecycleView
 import logging_config  # noinspection PyUnresolvedReferences
 import warnings
 
@@ -24,347 +23,6 @@ warnings.simplefilter(action="ignore", category=FutureWarning)
 Window.size = (960, 600)
 
 
-kv = """
-BoxLayout:
-    orientation: "vertical"
-    spacing: 8
-
-    TopSection:
-    MiddleSection:
-    BottomSection:
-
-
-<TopSection@BoxLayout>:
-    orientation: 'horizontal'
-    size_hint_y: None
-    height: '50dp'
-
-
-    # Strategy Selection
-    BoxLayout:
-        size_hint_x: 0.5
-        Label:
-            text: 'Strategy:'
-            size_hint_x: 0.25
-        Spinner:
-            id: strategy
-            text: 'Choose Strategy...'
-            values: ['RSI_Basic', 'RSI_Extended']
-            size_hint_x: 0.75
-            on_text: app.on_strategy_change(self, self.text)
-
-    # Balance Label
-    BoxLayout:
-        size_hint_x: 0.25
-        Label:
-            text: 'Balance:'
-        Label:
-            id: balance
-            on_parent: app.balance_label = self
-
-    # Mode Label
-    BoxLayout:
-        size_hint_x: 0.25
-        Label:
-            text: 'Auto-Pilot:'
-        Switch:
-            active: True
-
-<MiddleSection@BoxLayout>:
-    orientation: 'horizontal'
-    size_hint_y: None
-    height: '50dp'
-
-    # Symbol Selection and DCA Orders
-    BoxLayout:
-        size_hint_x: 0.5
-        Label:
-            text: 'Symbol:'
-        Spinner:
-            id: symbol
-            text: 'BTCUSDT'
-            values: ['BTCUSDT']
-        Label:
-            text: 'DCA Orders:'
-        Spinner:
-            id: dca_orders
-            text: '4'
-            values: [str(i) for i in range(1, 9)]
-
-    # Spacer
-    Widget:
-        size_hint_x: 0.25
-
-    # Start button
-    Button:
-        text: 'Start'
-        size_hint_x: 0.125
-        on_press: app.on_start_trading()
-
-    # Cancel button
-    Button:
-        text: 'Cancel'
-        size_hint_x: 0.125
-        on_press: app.on_cancel()
-
-
-<PositionListItem@BoxLayout>:
-    orientation: "horizontal"
-    size_hint_y: None
-    height: '30dp'
-    
-    symbol: ''
-    quantity: ''
-    entry_price: ''
-    mark_price: ''
-    liquidation_price: ''
-    pnl: ''
-    
-    canvas.before:
-        Color:
-            rgba: 1, 1, 1, 0.1  # Change this to the color you want for the line
-        Line:
-            points: [self.x, self.y, self.x + self.width, self.y]
-
-    Label:
-        text: root.symbol
-    Label:
-        text: root.quantity
-    Label:
-        text: root.entry_price
-    Label:
-        text: root.mark_price
-    Label:
-        text: root.liquidation_price
-    Label:
-        text: root.pnl
-
-
-<OrderListItem@BoxLayout>:
-    orientation: "horizontal"
-    size_hint_y: None
-    height: '30dp'
-    
-    open_time: ''
-    symbol: ''
-    order_type: ''
-    side: ''
-    price: ''
-    quantity: ''
-    realized_quantity: ''
-    status: ''
-    order_id: ''
-    
-    canvas.before:
-        Color:
-            rgba: 1, 1, 1, 0.1  # Change this to the color you want for the line
-        Line:
-            points: [self.x, self.y, self.x + self.width, self.y]
-
-    Label:
-        text: root.open_time
-        size_hint_x: 0.15
-    Label:
-        text: root.symbol
-        size_hint_x: 0.11
-    Label:
-        text: root.order_type
-        size_hint_x: 0.10
-    Label:
-        text: root.side
-        size_hint_x: 0.08
-    Label:
-        text: root.price
-        size_hint_x: 0.08
-    Label:
-        text: root.quantity
-        size_hint_x: 0.09
-    Label:
-        text: root.realized_quantity
-        size_hint_x: 0.09
-    Label:
-        text: root.status
-        size_hint_x: 0.15
-    Label:
-        text: root.order_id
-        size_hint_x: 0.15
-
-
-<BottomSection@BoxLayout>:
-    id: bottom_section
-    orientation: 'horizontal'
-
-    TabbedPanel:
-        do_default_tab: False
-
-        TabbedPanelItem:
-            text: 'Positions (' + str(app.position_count) + ')'
-            BoxLayout:
-                orientation: "vertical"
-                
-                BoxLayout:
-                    size_hint_y: None
-                    height: '30dp'
-                    canvas.before:
-                        Color:
-                            rgba: 1, 1, 1, 0.1  # Change this to the color you want for the line
-                        Line:
-                            points: [self.x, self.y, self.x + self.width, self.y]
-                    
-                    Label:
-                        text: 'Symbol'
-                    Label:
-                        text: 'Quantity'
-                    Label:
-                        text: 'Entry Price'
-                    Label:
-                        text: 'Mark Price'
-                    Label:
-                        text: 'Liquidation Price'
-                    Label:
-                        text: 'PnL'
-                RecycleView:
-                    id: positions_list
-                    data: app.position_data_list
-                    viewclass: 'PositionListItem'
-                    RecycleBoxLayout:
-                        default_size: None, dp(56)
-                        default_size_hint: 1, None
-                        size_hint_y: None
-                        height: self.minimum_height
-                        orientation: 'vertical'
-        TabbedPanelItem:
-            text: 'Orders (' + str(app.order_count) + ')'
-            BoxLayout:
-                orientation: "vertical"
-                BoxLayout:
-                    size_hint_y: None
-                    height: '30dp'
-                    
-                    canvas.before:
-                        Color:
-                            rgba: 1, 1, 1, 0.1  # Change this to the color you want for the line
-                        Line:
-                            points: [self.x, self.y, self.x + self.width, self.y]
-                    
-                    Label:
-                        text: 'Open Time'
-                        size_hint_x: 0.15
-                    Label:
-                        text: 'Symbol'
-                        size_hint_x: 0.11
-                    Label:
-                        text: 'Type'
-                        size_hint_x: 0.10
-                    Label:
-                        text: 'Side'
-                        size_hint_x: 0.08
-                    Label:
-                        text: 'Price'
-                        size_hint_x: 0.08
-                    Label:
-                        text: 'Quantity'
-                        size_hint_x: 0.09
-                    Label:
-                        text: 'Realized'
-                        size_hint_x: 0.09
-                    Label:
-                        text: 'Status'
-                        size_hint_x: 0.15
-                    Label:
-                        text: 'Order ID'
-                        size_hint_x: 0.15
-                RecycleView:
-                    id: orders_list
-                    data: app.order_data_list
-                    viewclass: 'OrderListItem'
-                    RecycleBoxLayout:
-                        default_size: None, dp(56)
-                        default_size_hint: 1, None
-                        size_hint_y: None
-                        height: self.minimum_height
-                        orientation: 'vertical'
-        TabbedPanelItem:
-            text: 'History O'
-            BoxLayout:
-                orientation: "vertical"
-                BoxLayout:
-                    size_hint_y: None
-                    height: '30dp'
-                    Label:
-                        text: 'Open Time'
-                        size_hint_x: 0.15
-                    Label:
-                        text: 'Symbol'
-                        size_hint_x: 0.11
-                    Label:
-                        text: 'Type'
-                        size_hint_x: 0.10
-                    Label:
-                        text: 'Side'
-                        size_hint_x: 0.08
-                    Label:
-                        text: 'Price'
-                        size_hint_x: 0.08
-                    Label:
-                        text: 'Quantity'
-                        size_hint_x: 0.09
-                    Label:
-                        text: 'Realized'
-                        size_hint_x: 0.09
-                    Label:
-                        text: 'Status'
-                        size_hint_x: 0.15
-                    Label:
-                        text: 'Order ID'
-                        size_hint_x: 0.15
-                RecycleView:
-                    id: history_list
-                    data: app.history_data_list
-                    viewclass: 'OrderListItem'
-                    RecycleBoxLayout:
-                        default_size: None, dp(56)
-                        default_size_hint: 1, None
-                        size_hint_y: None
-                        height: self.minimum_height
-                        orientation: 'vertical'
-        TabbedPanelItem:
-            text: 'History P'
-            BoxLayout:
-                orientation: "vertical"
-                BoxLayout:
-                    size_hint_y: None
-                    height: '30dp'
-                    Label:
-                        text: 'Symbol'
-                    Label:
-                        text: 'Quantity'
-                    Label:
-                        text: 'Entry Price'
-                    Label:
-                        text: 'Mark Price'
-                    Label:
-                        text: 'Liquidation Price'
-                    Label:
-                        text: 'PnL'
-                RecycleView:
-                    id: positions_list
-                    data: app.position_data_list
-                    viewclass: 'PositionListItem'
-                    RecycleBoxLayout:
-                        default_size: None, dp(56)
-                        default_size_hint: 1, None
-                        size_hint_y: None
-                        height: self.minimum_height
-                        orientation: 'vertical'
-        
-        TabbedPanelItem:
-            text: 'Logs'
-            Label:
-                text: 'Log data'
-"""
-
-
 class AsyncApp(App):
     balance_label = ObjectProperty(None)
     position_data_list = ListProperty([])
@@ -374,9 +32,12 @@ class AsyncApp(App):
     position_count = NumericProperty(0)
 
     def build(self):
-        return Builder.load_string(kv)
+        return Builder.load_file('src/gui/main.kv')
 
     def count_open_orders(self):
+        Logger.info(
+            "CALLING COUNT OPEN ORDERS: ORDER DATA LIST: %s", self.order_data_list
+        )
         if not self.order_data_list:  # check if order_data_list is empty
             self.order_count = 0
             return 0
@@ -388,10 +49,14 @@ class AsyncApp(App):
             )
             # Update order_count
             self.order_count = count
+            Logger.info("ORDER COUNT: %s", self.order_count)
             return count
 
     def count_open_positions(self):
-        Logger.info("CALLING COUNT OPEN POSITIONS")
+        Logger.info(
+            "CALLING COUNT OPEN POSITIONS: POSITION DATA LIST: %s",
+            self.position_data_list,
+        )
         if not self.position_data_list:  # check if position_data_list is empty
             self.position_count = 0
             return 0
@@ -411,8 +76,6 @@ class AsyncApp(App):
         Logger.info("Strategy: Chosen strategy is %s" % value)
 
     async def move_to_history(self, order_id):
-        # pause the execution for 3 seconds
-        await asyncio.sleep(3)
         # Find the order in the list
         for i, order in enumerate(self.order_data_list):
             if order["order_id"] == order_id:
@@ -442,7 +105,7 @@ class AsyncApp(App):
                     if key in order:
                         order[key] = value
                 # Reassign the list to trigger the UI update
-                self.order_data_list = self.order_data_list
+                # self.order_data_list = self.order_data_list
                 break
 
     async def update_ui(self):
@@ -466,15 +129,18 @@ class AsyncApp(App):
                         "pnl": str(data.pnl),
                     }
                 ]
-                self.count_open_positions()
+                self.position_data_list = self.position_data_list
 
             if isinstance(data, OrderData):
                 # Check if the order already exists
-                existing_orders = [
-                    order
-                    for order in self.order_data_list
-                    if order["order_id"] == data.order_id
-                ]
+                existing_orders = []
+
+                if self.order_data_list is not None:
+                    existing_orders = [
+                        order
+                        for order in self.order_data_list
+                        if order["order_id"] == data.order_id
+                    ]
                 if existing_orders:
                     # If it does, update it
                     self.update_order(
@@ -508,7 +174,8 @@ class AsyncApp(App):
                         "status": data.status,
                     }
                     self.order_data_list = self.order_data_list + [order_data]
-                self.count_open_orders()
+            self.count_open_orders()
+            self.count_open_positions()
 
     def app_func(self):
         """This will run both methods asynchronously and then block until they
