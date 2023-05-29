1d555609e84932474b5073dee59f2f78776f0a3e
omn88 <Mikolaj Nizio>
2023-05-29 20:54:01 +0200

    orders moved to history properly

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -10,9 +10,10 @@ from src.gui.async_app import AsyncApp
 def test_update_order(basic_rsi):
     async_app = AsyncApp()
     async_app.trading_system = basic_rsi
-    async_app.order_data_list = [
-        {
-            "order_id": "1",
+    order_id = "14"
+    async_app.open_orders = {
+        order_id: {
+            "order_id": order_id,
             "open_time": "12345",
             "symbol": "BTC",
             "order_type": "LIMIT",
@@ -22,33 +23,48 @@ def test_update_order(basic_rsi):
             "realized_quantity": "0",
             "status": "NEW",
         }
-    ]
-    async_app.update_order(order_id="1", price="20")
-    assert async_app.order_data_list[0]["price"] == "20"
+    }
+
+    assert async_app.open_orders[order_id]["price"] == "10"
+
+    async_app.update_order(
+        data=OrderData(
+            order_id=order_id,
+            price="20",
+            open_time="12345",
+            symbol="BTC",
+            order_type="LIMIT",
+            side="BUY",
+            quantity="1",
+            realized_quantity="0",
+            status="NEW",
+        )
+    )
+    assert async_app.open_orders[order_id]["price"] == "20"
 
 
-def test_count_open_orders(basic_rsi):
-    async_app = AsyncApp()
-    async_app.trading_system = basic_rsi
-    async_app.order_data_list = [
-        {"order_id": "1", "status": "NEW"},
-        {"order_id": "2", "status": "PARTIALLY_FILLED"},
-        {"order_id": "3", "status": "FILLED"},
-    ]
-    count = async_app.count_open_orders()
-    assert count == 2
-
-
-def test_count_open_positions(basic_rsi):
-    async_app = AsyncApp()
-    async_app.trading_system = basic_rsi
-    async_app.position_data_list = [
-        {"symbol": "BTC", "quantity": "1"},
-        {"symbol": "ETH", "quantity": "0"},
-        {"symbol": "LTC", "quantity": "0.5"},
-    ]
-    count = async_app.count_open_positions()
-    assert count == 2
+# def test_count_open_orders(basic_rsi):
+#     async_app = AsyncApp()
+#     async_app.trading_system = basic_rsi
+#     async_app.open_orders = {
+#         "1": {"order_id": "1", "status": "NEW"},
+#         "2": {"order_id": "2", "status": "PARTIALLY_FILLED"},
+#         "3": {"order_id": "3", "status": "FILLED"}
+#     }
+#     count = async_app.order_count
+#     assert count == 2
+#
+#
+# def test_count_open_positions(basic_rsi):
+#     async_app = AsyncApp()
+#     async_app.trading_system = basic_rsi
+#     async_app.open_positions = {
+#         "BTC": {"symbol": "BTC", "quantity": "1"},
+#         "ETH": {"symbol": "ETH", "quantity": "0"},
+#         "LTC": {"symbol": "LTC", "quantity": "0.5"},
+#     }
+#     count = async_app.position_count
+#     assert count == 2
 
 
 async def test_update_ui(basic_rsi, mock_AsyncClient):
@@ -86,5 +102,47 @@ async def test_update_ui(basic_rsi, mock_AsyncClient):
     await async_app.update_ui()
     # Check if the data has been processed
     assert async_app.balance_label == "1000 USDT"
-    assert len(async_app.position_data_list) == 1
-    assert len(async_app.order_data_list) == 1
+    assert len(async_app.open_positions) == 1
+    assert len(async_app.open_orders) == 1
+
+
+async def test_update_ui_with_multiple_orders_one_filled(mock_AsyncClient):
+    async_app = AsyncApp()
+    async_app.trading_system = MagicMock()
+    async_app.ui_queue = asyncio.Queue()
+    async_app.logger = MagicMock()
+
+    # Create 4 new orders
+    for i in range(1, 5):
+        order_data = OrderData(
+            order_id=str(i),
+            open_time="12345",
+            symbol="BTC",
+            order_type="LIMIT",
+            side="BUY",
+            price="10",
+            quantity="1",
+            realized_quantity="0",
+            status="NEW",
+        )
+        await async_app.ui_queue.put(order_data)
+
+    await async_app.ui_queue.put(EventName.SENTINEL)
+
+    # Mock the logger
+    async_app.logger = MagicMock()
+    # Run the update_ui() method once to process all orders
+    await async_app.update_ui()
+
+    # Now mark the first order as filled
+    first_order = async_app.open_orders["1"]
+    first_order["status"] = "FILLED"
+    first_order["realized_quantity"] = "1"
+    await async_app.ui_queue.put(first_order)
+    await async_app.update_ui()
+
+    # Check if the data has been processed
+    assert len(async_app.open_positions) == 1
+    assert len(async_app.open_orders) == 4
+    assert async_app.closed_orders["1"].status == "FILLED"
+    assert len(async_app.closed_orders) == 1
