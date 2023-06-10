8de2e4be3050d598287cbfbe934b2c4377cc3973
omn88 <Mikolaj Nizio>
2023-06-10 15:42:58 +0200

    first cerebro run with results

--- src/backtest/backtrader_check.py
+++ src/backtest/backtrader_check.py
@@ -1,74 +1,105 @@
 import backtrader as bt
 import pandas as pd
-import json
-from datetime import datetime
+from backtrader.feeds import PandasData
+import logging
+import logging_config
+
+logger = logging.getLogger("backtrader")
 
 
-class CrossOver(bt.Strategy):
+class CustomRSISignal(bt.Indicator):
+    lines = (
+        "buy_signal",
+        "sell_signal",
+    )
+    params = (
+        ("rsi_low1", 30),
+        ("rsi_low2", 20),
+        ("rsi_high1", 70),
+        ("rsi_high2", 80),
+        ("dca_orders", 4),
+        ("dca_span", 0.005),
+        ("value", 4),
+    )
+
     def __init__(self):
-        self.sma = bt.ind.SMA(period=50)
-        self.crossover = bt.ind.CrossOver(self.data.close, self.sma)
+        self.rsi = bt.ind.RSI(self.data.close)
+        super(CustomRSISignal, self).__init__()
 
     def next(self):
-        if not self.position:  # not in the market
-            if self.crossover > 0:  # if fast crosses slow to the upside
-                self.buy()  # enter long
-        elif self.crossover < 0:  # in the market & cross to the downside
-            self.close()  # close long position
+        # Buy signals
+        if (
+            self.rsi[-3] < self.p.rsi_low1
+            and self.rsi[-2] > self.p.rsi_low1
+            and self.rsi[-1] > self.p.rsi_low1
+        ):
+            self.lines.buy_signal[0] = True
+        elif self.rsi[-2] < self.p.rsi_low2 and self.rsi[-1] > self.p.rsi_low2:
+            self.lines.buy_signal[0] = True
+        else:
+            self.lines.buy_signal[0] = False
+
+        # Sell signals
+        if (
+            self.rsi[-3] > self.p.rsi_high1
+            and self.rsi[-2] < self.p.rsi_high1
+            and self.rsi[-1] < self.p.rsi_high1
+        ):
+            self.lines.sell_signal[0] = True
+        elif self.rsi[-2] > self.p.rsi_high2 and self.rsi[-1] < self.p.rsi_high2:
+            self.lines.sell_signal[0] = True
+        else:
+            self.lines.sell_signal[0] = False
 
 
-# Create a backtest
-cerebro = bt.Cerebro()
-cerebro.addstrategy(CrossOver)
-
-# Load the JSON data
-with open("data/BTCUSDT/15m_historical_klines.json", "r") as f:
-    data_dict = json.load(f)
-
-# Convert the JSON data to a pandas DataFrame
-data_df = pd.DataFrame(data_dict)
-data_df["open_time"] = pd.to_datetime(data_df["open_time"])
-data_df.set_index("open_time", inplace=True)
-
-# Convert string data to float
-for col in ["open", "high", "low", "close", "volume"]:
-    data_df[col] = data_df[col].astype(float)
-
-
-class CustomPandasData(bt.feeds.PandasData):
-    # Define the 'lines' (fields) that your data feed will provide
-    lines = (
-        "open",
-        "high",
-        "low",
-        "close",
-        "volume",
-        "taker_buy_base_asset_volume",
-        "taker_buy_quote_asset_volume",
-    )
-
-    # Define parameters for 'lines'
+class StrategyRsiExtended(bt.Strategy):
     params = (
-        ("datetime", None),  # pandas datetime column (index)
-        ("open", -1),
-        ("high", -1),
-        ("low", -1),
-        ("close", -1),
-        ("volume", -1),
-        ("openinterest", None),
-        ("taker_buy_base_asset_volume", -1),
-        ("taker_buy_quote_asset_volume", -1),
+        ("dca_orders", 4),
+        ("dca_span", 0.005),
+        ("value", 4),
     )
 
+    def __init__(self):
+        self.rsi_signal = CustomRSISignal(self.data)
 
-# Create a Data Feed
-data = CustomPandasData(dataname=data_df)
+    def next(self):
+        if self.position.size == 0:  # check if there is an open position
+            if self.rsi_signal.buy_signal[0] == 1:
+                order_price = self.data.close[0]
+                for i in range(self.p.dca_orders):
+                    self.buy(
+                        price=order_price - self.p.dca_span * i,
+                        size=self.p.value,
+                        exectype="Order.Limit",
+                    )
+            elif self.rsi_signal.sell_signal[0] == 1:
+                order_price = self.data.close[0]
+                for i in range(self.p.dca_orders):
+                    self.sell(
+                        price=order_price + self.p.dca_span * i,
+                        size=self.p.value,
+                        exectype="Order.Limit",
+                    )
 
-# Add the Data Feed to Cerebro
+
+class PandasDataWithSignals(PandasData):
+    lines = ("rsi_signal",)
+    params = (("rsi_signal", -1),)
+
+
+cerebro = bt.Cerebro()
+
+# Load the CSV file into a pandas DataFrame
+df = pd.read_csv("data/BTCUSDT/test.csv")
+df["datetime"] = pd.to_datetime(df["datetime"])
+df.set_index("datetime", inplace=True)
+
+
+# Create a data feed
+data = PandasDataWithSignals(dataname=df)
 cerebro.adddata(data)
-
-# Run the backtest
+cerebro.addstrategy(StrategyRsiExtended)
 cerebro.run()
 
-# Plot the result
 cerebro.plot(style="candlestick")
+logger.info("DONE")
