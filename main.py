70a80cbb5a5d9eaa9836436f78e07714d14cc82f
omn88 <Mikolaj Nizio>
2023-12-17 16:39:48 +0100

    refactor + add docstrings to the main module

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 """
-Cryptocopter main module.
+This is the main module of the Cryptocopter project.
+
+This module initializes the Kivy application and starts the main event loop. It sets up
+the initial window size and configures the logging settings. It also defines the main
+asynchronous function that creates an instance of AsyncApp, which is responsible for
+managing the trading systems and the user interface.
+
+Functions:
+    main: The main function of the module. It creates an instance of AsyncApp and starts
+    the application's main event loop.
 """
 
 import os
-from src.common.common import create_async_client
+import asyncio
+import warnings
+import logging_config  # noinspection PyUnresolvedReferences
 
 os.environ["KIVY_LOG_MODE"] = "MIXED"
-import asyncio
 
 from kivy.core.window import Window
-import logging_config  # noinspection PyUnresolvedReferences
-import warnings
+from src.common.common import create_async_client
 from src.gui.async_app import AsyncApp
 
-
 warnings.simplefilter(action="ignore", category=FutureWarning)
-
 
 # Set initial window size
 Window.size = (960, 600)
 
 
 async def main():
+    """
+    The main function of the module.
+
+    This function creates an instance of AsyncApp and starts the application's main event loop.
+
+    Returns:
+        None
+    """
     app = AsyncApp(client=await create_async_client())
     await app.async_run()
 
