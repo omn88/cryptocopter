53e4ab87f1f0ca32bdb84bcfa640fee40647e0e6
omn88 <Mikolaj Nizio>
2023-12-18 20:31:11 +0100

    add docstrings to the async app module

--- src/gui/async_app.py
+++ src/gui/async_app.py
@@ -1,15 +1,16 @@
+"""Main module for managing trading strategies.
+
+This module contains the `AsyncApp` class, which is responsible for creating and managing instances
+of `TradingSystem` and `StrategyTab` for each trading strategy. It also sets up a logging handler
+for each strategy.
+"""
+
 import asyncio
 import logging
 from kivy.app import App
-from kivy.clock import Clock
 from kivy.lang import Builder
 from kivy.logger import Logger
-from kivy.properties import (
-    ListProperty,
-    StringProperty,
-    ObjectProperty,
-    NumericProperty,
-)
+from kivy.properties import ListProperty
 from kivy.uix.tabbedpanel import TabbedPanelItem
 from logging_config import KivyGuiHandler
 from src.common.identifiers import BinanceClient
@@ -20,6 +21,17 @@ logger = logging.getLogger("async_app")
 
 
 class AsyncApp(App):
+    """Main application class for managing trading strategies.
+
+    This class is responsible for creating and managing instances of `TradingSystem` and `StrategyTab`
+    for each trading strategy. It also sets up a logging handler for each strategy.
+
+    Attributes:
+        strategy_tabs (ListProperty): A list of `StrategyTab` instances for each strategy.
+        trading_systems (ListProperty): A list of `TradingSystem` instances for each strategy.
+        strategy_mapping (dict): A mapping from strategy names to abbreviations.
+    """
+
     strategy_tabs = ListProperty([])
     trading_systems = ListProperty([])
 
@@ -32,11 +44,23 @@ class AsyncApp(App):
     }
 
     def __init__(self, client: BinanceClient, **kwargs):
+        """Initializes the `AsyncApp` instance.
+
+        Args:
+            client (BinanceClient): The Binance client to use for trading.
+            **kwargs: Additional keyword arguments.
+        """
         super(AsyncApp, self).__init__(**kwargs)
         self.trading_systems = []
         self.client = client
 
     def setup_logging_handler(self, strategy_logger, log_display_widget):
+        """Sets up a logging handler for a strategy.
+
+        Args:
+            strategy_logger (Logger): The logger to set up the handler for.
+            log_display_widget (Widget): The widget to display the logs in.
+        """
         gui_log_handler = KivyGuiHandler(log_display_widget)
         formatter = logging.Formatter(
             "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -48,18 +72,31 @@ class AsyncApp(App):
         strategy_logger.info("Logging handler configured with success")
 
     def build(self):
+        """Builds the application.
+
+        Returns:
+            Widget: The root widget of the application.
+        """
         Builder.load_file("src/gui/common_widgets.kv")
         Builder.load_file("src/gui/strategytab.kv")
         self.root = Builder.load_file("src/gui/main.kv")
         return self.root
 
     def log_spinner_change(self, spinner, new_value):
+        """Logs a message when a spinner value changes.
+
+        Args:
+            spinner (str): The name of the spinner.
+            new_value (str): The new value of the spinner.
+        """
         Logger.info("%s spinner value changed to %s", spinner, new_value)
 
     def start_strategy(self):
+        """Starts a new strategy."""
         asyncio.create_task(self.on_start_strategy())
 
     async def on_start_strategy(self):
+        """Creates and starts a new trading strategy."""
         # Check if a strategy and symbol are selected
         strategy = self.root.ids.strategy_spinner.text
         symbol = self.root.ids.symbol_spinner.text
