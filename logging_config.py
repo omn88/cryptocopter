caebafe2c32d74394e0d2f7c5c2760d236abb1a2
omn88 <Mikolaj Nizio>
2024-06-14 14:37:44 +0200

    part ci

--- logging_config.py
+++ logging_config.py
@@ -21,10 +21,8 @@ now = datetime.now()
 
 # setup basic config for all loggers
 logging.basicConfig(
-    level=logging.INFO,
-    filename="artifacts/rsi_based_futures_{}.log".format(
-        now.strftime("%Y-%m-%d_%H-%M-%S")
-    ),
+    level=logging.DEBUG,
+    filename=f"artifacts/cryptocopter_{now.strftime('%Y-%m-%d_%H-%M-%S')}.log",
     filemode="w",
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
@@ -32,7 +30,7 @@ logging.basicConfig(
 
 # create a console handler
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
+console_handler.setLevel(logging.DEBUG)
 console_handler.setFormatter(
     logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 )
