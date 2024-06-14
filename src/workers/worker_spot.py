361e66f015cbad1ee7a75df2b6fe8d6f7d7ccb2a
omn88 <Mikolaj Nizio>
2024-06-14 14:32:03 +0200

    logging quite decreased

--- src/workers/worker_spot.py
+++ src/workers/worker_spot.py
@@ -1,4 +1,5 @@
 from logging_config import StrategyLogger
+from src.common.identifiers.common import SentinelUpdate
 from src.common.identifiers.spot import (
     AccountPosition,
     EventName,
@@ -13,21 +14,23 @@ from src.workers.trading_state_machine import TradingStateMachine
 
 async def worker(state_machine: TradingStateMachine, logger: StrategyLogger):
     while True:
-        logger.info(
+        logger.debug(
             "-------------------------------------POSITION-----------------------------------------"
         )
         if state_machine.strategy.queue.qsize() == 0:
-            logger.info("Awaiting new Event...")
+            logger.debug("Awaiting new Event...")
 
         event = await state_machine.strategy.queue.get()
         assert isinstance(event, Event)
+
+        logger.debug("New event: %s", event)
 
         if EventName.TICKER == event.name:
             assert isinstance(event.content, TickerUpdate)
             assert isinstance(state_machine.strategy, HpManager)
             state_machine.strategy.ticker_update = event.content
 
-            logger.info(
+            logger.debug(
                 "Last price for %s: %s, Order trigger price: %s",
                 state_machine.strategy.ticker_update.symbol,
                 state_machine.strategy.ticker_update.last_price,
@@ -37,26 +40,22 @@ async def worker(state_machine: TradingStateMachine, logger: StrategyLogger):
             await state_machine.strategy.process_ticker()  # type: ignore
 
         elif EventName.EXECUTION_REPORT == event.name:
-            logger.info("Entering order event: %s", event)
             assert isinstance(event.content, ExecutionReport)
-            state_machine.strategy.order_update = event.content
+            state_machine.strategy.execution_report = event.content
             await state_machine.strategy.process_order()  # type: ignore
 
         elif EventName.ACCOUNT_POSITION == event.name:
-            logger.info("Entering account event: %s", event)
             assert isinstance(event.content, AccountPosition)
-            state_machine.strategy.account_update = event.content
+            state_machine.strategy.account_position = event.content
             await state_machine.strategy.process_account()  # type: ignore
 
         elif EventName.SIGNAL == event.name:
-            logger.info("Entering signal event: %s", event)
             assert isinstance(event.content, SignalUpdate)
             state_machine.strategy.signal_update = event.content
             await state_machine.strategy.process_signal()  # type: ignore
 
         elif EventName.SENTINEL == event.name:
-            logger.info("Entering sentinel event -> Exiting worker")
-
+            assert isinstance(event.content, SentinelUpdate)
             await state_machine.strategy.position_handler.cancel_position()
             return
 
