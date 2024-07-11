93aa0f5fd86e73d145ef5ba6e94664f44f09d0f6
omn88 <Mikolaj Nizio>
2024-07-11 09:35:33 +0200

    move price range to the searchable dropdown

--- src/gui/searchable_drop_down.py
+++ src/gui/searchable_drop_down.py
@@ -1,4 +1,5 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
@@ -14,20 +15,46 @@ class SearchableDropDown(BoxLayout):
         self.orientation = "vertical"
         self.dropdown = DropDown()
 
-        self.search_input = TextInput(size_hint_y=None, height=30, multiline=False)
+        # Adding search label and input fields
+        search_box = BoxLayout(orientation="horizontal", size_hint_y=None, height=30)
+        search_box.add_widget(Label(text="Search:", size_hint_x=0.2))
+        self.search_input = TextInput(size_hint_x=0.8, multiline=False)
         self.search_input.bind(text=self.update_dropdown)
-        self.add_widget(self.search_input)
+        search_box.add_widget(self.search_input)
+        self.add_widget(search_box)
 
+        # Adding main button for dropdown
         self.main_button = Button(text="Select Symbol", size_hint_y=None, height=30)
         self.main_button.bind(on_release=self.dropdown.open)
         self.add_widget(self.main_button)
 
+        # Binding dropdown selection
         self.dropdown.bind(
             on_select=lambda instance, x: setattr(self.main_button, "text", x)
         )
         self.dropdown.bind(
             on_select=lambda instance, x: setattr(self, "selected_value", x)
         )
+        self.dropdown.bind(on_select=self.update_prices)
+
+        # Adding price input fields in a single horizontal BoxLayout
+        price_box = BoxLayout(orientation="horizontal", size_hint_y=None, height=30)
+        price_box.add_widget(Label(text="Price Low:", size_hint_x=0.2))
+        self.price_low_input = TextInput(
+            hint_text="0.0", size_hint_x=0.3, height=30, multiline=False
+        )
+        price_box.add_widget(self.price_low_input)
+        price_box.add_widget(Label(text="Price High:", size_hint_x=0.2))
+        self.price_high_input = TextInput(
+            hint_text="0.0", size_hint_x=0.3, height=30, multiline=False
+        )
+        price_box.add_widget(self.price_high_input)
+        self.add_widget(price_box)
+
+    def update_prices(self, instance, value):
+        # Example: set static values for demo purposes
+        self.price_low_input.text = "666"
+        self.price_high_input.text = "999"
 
     def update_dropdown(self, instance, value):
         self.dropdown.clear_widgets()
